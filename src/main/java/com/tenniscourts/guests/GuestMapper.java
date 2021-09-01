8b15e13d5c920860e71b10f8406ff5d3f9fc620e
diegoalexandrepags <tqi_dsouza@uolinc.com>
2021-09-01 12:11:16 -0300

    First stable version

--- src/main/java/com/tenniscourts/guests/GuestMapper.java
+++ src/main/java/com/tenniscourts/guests/GuestMapper.java
@@ -1,13 +1,16 @@
-package com.tenniscourts.tenniscourts;
+package com.tenniscourts.guests;
 
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
-public interface TennisCourtMapper {
-    TennisCourtDTO map(TennisCourt source);
+public interface GuestMapper {
+    GuestDTO map(Guest source);
 
+    /*
+    * Maps for two directions
+    * */
     @InheritInverseConfiguration
-    TennisCourt map(TennisCourtDTO source);
+    Guest map(GuestDTO source);
 }
--- src/test/java/com/tenniscourts/reservations/ReservationServiceTest.java
+++ src/test/java/com/tenniscourts/reservations/ReservationServiceTest.java
@@ -1,12 +1,17 @@
 package com.tenniscourts.reservations;
 
+import com.tenniscourts.guests.GuestMapper;
+import com.tenniscourts.guests.GuestService;
 import com.tenniscourts.schedules.Schedule;
+import com.tenniscourts.schedules.ScheduleMapper;
+import com.tenniscourts.schedules.ScheduleService;
 import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
@@ -23,6 +28,24 @@ public class ReservationServiceTest {
     @InjectMocks
     ReservationService reservationService;
 
+    @Mock
+    private ReservationRepository repository;
+
+    @Mock
+    private ReservationMapper mapper;
+
+    @Mock
+    private ScheduleService scheduleService;
+
+    @Mock
+    private ScheduleMapper scheduleMapper;
+
+    @Mock
+    private GuestService guestService;
+
+    @Mock
+    private GuestMapper guestMapper;
+
     @Test
     public void getRefundValueFullRefund() {
         Schedule schedule = new Schedule();
@@ -33,4 +56,26 @@ public class ReservationServiceTest {
 
         Assert.assertEquals(reservationService.getRefundValue(Reservation.builder().schedule(schedule).value(new BigDecimal(10L)).build()), new BigDecimal(10));
     }
+
+    @Test
+    public void refundTest() {
+        var schedule = Schedule.builder().build();
+
+        var startDateTime = LocalDateTime.now().plusDays(2);
+        schedule.setStartDateTime(startDateTime);
+        Assert.assertEquals(BigDecimal.TEN, reservationService.getRefundValue(Reservation.builder().schedule(schedule).value(BigDecimal.TEN).build()));
+
+        startDateTime = LocalDateTime.now().plusHours(1).plusMinutes(30);
+        schedule.setStartDateTime(startDateTime);
+        Assert.assertEquals(BigDecimal.valueOf(2.5).setScale(2), reservationService.getRefundValue(Reservation.builder().schedule(schedule).value(BigDecimal.TEN).build()));
+
+        startDateTime = LocalDateTime.now().plusHours(9).plusMinutes(30);
+        schedule.setStartDateTime(startDateTime);
+        Assert.assertEquals(BigDecimal.valueOf(5).setScale(1), reservationService.getRefundValue(Reservation.builder().schedule(schedule).value(BigDecimal.TEN).build()));
+
+        startDateTime = LocalDateTime.now().plusHours(11).plusMinutes(30);
+        schedule.setStartDateTime(startDateTime);
+        Assert.assertEquals(BigDecimal.valueOf(5.00).setScale(2), reservationService.getRefundValue(Reservation.builder().schedule(schedule).value(BigDecimal.TEN).build()).setScale(2));
+    }
+
 }
